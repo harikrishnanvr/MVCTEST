46b50e80b0116742401d853c21c7dcfe0f205a5c
harikrishnanvr <djharry123@gmail.com>
2017-03-23 17:51:38 +0530

    App Created

---
+++ MVCTrials/Controllers/MyInfoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTrials.Controllers
+{
+    public class MyInfoController : Controller
+    {
+       public ActionResult MyInfo()
+       {
+            Models.Customer objCustomer = new Models.Customer();
+            objCustomer.Name = "Hari";
+            objCustomer.Age = 28;
+            objCustomer.Nationality = "India";
+
+            return View("MyInfo",objCustomer);
+       }
+        
+    }
+}
---
+++ MVCTrials/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCTrials.MvcApplication" Language="C#" %>
